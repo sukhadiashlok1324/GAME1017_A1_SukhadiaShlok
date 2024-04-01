5117866427926a485d9a164d1388b7034bfbbc24
sukhadiashlok1324 <101522836@georgebrown.ca>
2024-04-01 11:58:31 -0400

    Score Added

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     private int score = 0;
 
     private void Start()
